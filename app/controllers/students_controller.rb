5e85b77cea51820a654f070535b24bfe344be9e8
NickeaBennett <nickeabennett@gmail.com>
2022-08-16 14:19:17 -0400

    commit tree d36c8994a3d82142c46755c7754af9d5b7979f3f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def student_id
+    students_id = Student.find(params[:id])
+    render json: students_id
+  end
+
 end
